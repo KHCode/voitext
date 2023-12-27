8d75ae80f08fa6529b12f476160d7ff8e1f0915e
Kris Hill <krish@ricksteves.com>
2023-12-27 14:14:34 -0800

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def show
+    # Your code here
+  end
+
+  def create
+    # Your code here
+  end
+
+  def update
+    # Your code here
+  end
+
+  def destroy
+    # Your code here
+  end
+end
